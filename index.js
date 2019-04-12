41aa47ea1631f3379c3647a24312a850c7d99b7e
Jacob Queen <jacobsq@protonmail.com>
2019-04-11 20:20:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, world! [helloworld sample]');
+    res.end('Welcome to ArchTechnica\nThis site is currently under construction\nIt will eventually host all of my personal projects.');
 }).listen(process.env.PORT);  
