c874ada3941312126b963920bee4df254f08f27e
Jacob Queen <jacobsq@protonmail.com>
2019-04-15 00:02:46 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,27 @@
 var button;
-var angle = PI/4;
+var autoButton;
+var rateSlider;
+var angle;
+var autorun;
+var rate;
 
 function setup() 
 {
+    angle = PI/4;
+    rate = 0.01;
+    autorun = false;
     createCanvas(window.innerWidth-20, window.innerHeight-20);
     slider = createSlider(0, TWO_PI, PI/4, 0.01);
-    slider.position(width / 2 - 80, height / 2 + 10);
+    rateSlider = createSlider(0, 0.10, 0.01, 0.01);
+    slider.position(width / 2 - width / 20, height / 2 + height / 80);
+    rateSlider.position(width / 2 + width / 14, height / 2 + height / 25);
     button = createButton('More information...');
-    button.position(width / 2 - 65, height/2 + 30);
+    button.size(134,21);
+    autoButton = createButton('Auto On');
+    button.position(width / 2 - width / 16, height/2 + height / 25);
     button.mousePressed(info);
+    autoButton.position(width / 2 + width / 38, height / 2 + height / 25);
+    autoButton.mousePressed(setAuto);
 }
 
 function resize_canvas()
@@ -26,30 +39,29 @@ function resize_canvas()
   
 function draw() 
 {
-    // background
+    if (autorun) auto();
     if (angle != slider.value())
     {
-        angle = slider.value();
+        if (!autorun) angle = slider.value();
         background(51);
         stroke(255);
         fill(255, 255, 255);
         textAlign(CENTER);
-        textSize(18);
-        text((angle / PI).toFixed(4) + " PI", width/2, height/2+60);
+        textSize(24*height/900);
+        text((angle / PI).toFixed(4) + " PI", width/2, height/2+height/12);
         textFunc();
-        text()
         translate(width/2, height);
-        branch(100);
+        branch(height / 7 - height / 216);
     }
 }
 
 function textFunc()
 {
-    textSize(32);
+    textSize(48*height/900);
     fill(255, 255, 255);
     textAlign(CENTER);
-    text('Welcome to ArchTechnica', width/2, height/2 - 80);
-    textSize(18);
+    text('Welcome to ArchTechnica', width/2, height/2 - height / 4);
+    textSize(24*height/900);
     text('Play with the slider you can see below this text.', width/2, height/2);
 }
 
@@ -74,7 +86,30 @@ function info()
 {
     fill(255, 255, 255);
     textAlign(CENTER);
-    textSize(18);
-    text('You can see that the slider is\nmanipulating a fractal tree.', width-width/4, height/2);
+    textSize(24*height/900);
+    text(`You can see that the slider is\nmanipulating a fractal tree.
+          Unfortunately this is a very computationally intensive algorithm.
+          Not very good for javascript or browsers.`, width-width/4 + width / 192, height/2 + height / 27);
+}
+
+function setAuto()
+{
+    if (autorun == false) 
+    {
+        autorun = true;
+        autoButton.html('Auto Off');
+    }
+    else if (autorun == true)
+    {
+        autorun = false;
+        autoButton.html('Auto On');
+    }
+}
+
+function auto()
+{
+    if (rate != rateSlider.value()) rate = rateSlider.value();
+    if (angle <= TWO_PI) angle += rate;
+    else angle = 0;
 }
 
